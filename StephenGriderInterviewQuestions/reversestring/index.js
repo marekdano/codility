86b484c84fa1022a32a389487d4d02c3af3441ee
Marek Dano <mk.dano@gmail.com>
2018-01-25 16:08:27 +0000

    Add reverse string solution.

---
+++ StephenGriderInterviewQuestions/reversestring/index.js
@@ -0,0 +1,32 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split("").reduce((revstr, char) => char + revstr, "");
+}
+
+module.exports = reverse;
+
+// other solutions
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//
+//   return reversed;
+// }
