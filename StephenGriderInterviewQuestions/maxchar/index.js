40e501c16a2d4fcda032cc347513b4e1b89993f0
Marek Dano <mk.dano@gmail.com>
2018-01-28 18:25:24 +0000

    Add solution of maximum character occurence (maxchar).

---
+++ StephenGriderInterviewQuestions/maxchar/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const charMap = {};
+  let maxChar = "";
+  let maxValue = 0;
+
+  for (let char of str) {
+    // if (charMap[char]) {
+    //   charMap[char] += 1;
+    // } else {
+    //   charMap[char] = 1;
+    // }
+    // simplified
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > maxValue) {
+      maxValue = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+module.exports = maxChar;
