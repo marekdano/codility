efc974b053873d22da31f728415af09eb20a0075
Marek Dano <mk.dano@gmail.com>
2018-02-02 21:40:42 +0000

    Add another solution to the anagram problem.

--- StephenGriderInterviewQuestions/anagrams/index.js
+++ StephenGriderInterviewQuestions/anagrams/index.js
@@ -9,39 +9,54 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 const anagrams = (stringA, stringB) => {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  return true;
+  return cleanString(stringA) === cleanString(stringB);
 };
 
-const buildCharMap = str => {
-  // const charMap = {};
-
-  // for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-  //   charMap[char] = charMap[char] + 1 || 1;
-  // }
-
-  // return charMap;
-
+const cleanString = str => {
   return str
     .replace(/[^\w]/g, "")
     .toLowerCase()
     .split("")
-    .reduce((prev, char) => {
-      prev[char] = prev[char] + 1 || 1;
-      return prev;
-    }, {});
+    .sort()
+    .join("");
 };
 
 module.exports = anagrams;
+
+// other solution
+
+// const anagrams = (stringA, stringB) => {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
+// const buildCharMap = str => {
+//   // const charMap = {};
+
+//   // for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//   //   charMap[char] = charMap[char] + 1 || 1;
+//   // }
+
+//   // return charMap;
+
+//   return str
+//     .replace(/[^\w]/g, "")
+//     .toLowerCase()
+//     .split("")
+//     .reduce((prev, char) => {
+//       prev[char] = prev[char] + 1 || 1;
+//       return prev;
+//     }, {});
+// };
