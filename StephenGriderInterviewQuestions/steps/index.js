af0e5935b7ae930d35c8a534096ed4cc532a8b55
Marek Dano <mk.dano@gmail.com>
2018-02-08 21:12:27 +0000

    Add recursive solution to the steps problem.

--- StephenGriderInterviewQuestions/steps/index.js
+++ StephenGriderInterviewQuestions/steps/index.js
@@ -17,18 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let line = "";
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        line += "#";
-      } else {
-        line += " ";
-      }
-    }
-    console.log(line);
+function steps(n, row = 0, level = "") {
+  if (n === row) {
+    return;
   }
+
+  if (n === level.length) {
+    console.log(level);
+    return steps(n, row + 1);
+  }
+
+  if (level.length <= row) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+
+  return steps(n, row, level);
 }
 
 module.exports = steps;
+
+// other solution
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
