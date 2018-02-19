2ee02e395dda2edf7895eaeafc3e95bc7dfaec4e
Marek Dano <mk.dano@gmail.com>
2018-02-19 21:57:22 +0000

    Add solution with memoization to the fib problem.

--- StephenGriderInterviewQuestions/fib/index.js
+++ StephenGriderInterviewQuestions/fib/index.js
@@ -8,7 +8,21 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   // if (n === 0) {
   //   return 0;
   // } else if (n === 1) {
@@ -21,6 +35,8 @@ function fib(n) {
 
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
 
