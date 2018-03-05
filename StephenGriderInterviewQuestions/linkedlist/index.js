861df38006e71e61f4f3bb5aef8f73bc5ea53a5b
Marek Dano <mk.dano@gmail.com>
2018-03-05 20:42:08 +0000

    Add insertFirst method to the linkedList.

--- StephenGriderInterviewQuestions/linkedlist/index.js
+++ StephenGriderInterviewQuestions/linkedlist/index.js
@@ -13,6 +13,13 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertFirst(data) {
+    //const node = new Node(data, this.head);
+    //this.head = node;
+    // simplified
+    this.head = new Node(data, this.head);
+  }
 }
 
 module.exports = { Node, LinkedList };
