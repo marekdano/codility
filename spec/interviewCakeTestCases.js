3a329b38f68e17ca5c1ba467c155b41905ae606c
Marek Dano <mk.dano@gmail.com>
2018-03-08 20:21:30 +0000

    Add more test cases for Find_k-th_inLinkedList.

--- spec/interviewCakeTestCases.js
+++ spec/interviewCakeTestCases.js
@@ -44,7 +44,7 @@ describe("searchPoint function", () => {
     expect(searchPoint).to.be.a("function");
   });
 
-  it("exists", () => {
+  it("searchPoint should be in position 0", () => {
     expect(searchPoint([])).to.equal(0);
   });
 
@@ -97,5 +97,69 @@ describe("kthToLastNode function", () => {
     d.next = e;
 
     expect(kthToLastNode(2, a).value).to.equal("Devil's Food");
+  });
+
+  it("should return node with value Angel Food", () => {
+    let a = new LinkedListNode("Angel Food");
+    let b = new LinkedListNode("Bundt");
+    let c = new LinkedListNode("Cheese");
+    let d = new LinkedListNode("Devil's Food");
+    let e = new LinkedListNode("Eccles");
+
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    d.next = e;
+
+    expect(kthToLastNode(5, a).value).to.equal("Angel Food");
+  });
+
+  it("should return node with value Devil's Food", () => {
+    let a = new LinkedListNode("Angel Food");
+    let b = new LinkedListNode("Bundt");
+    let c = new LinkedListNode("Cheese");
+    let d = new LinkedListNode("Devil's Food");
+    let e = new LinkedListNode("Eccles");
+
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    d.next = e;
+
+    expect(kthToLastNode(1, a).value).to.equal("Eccles");
+  });
+
+  it("should throw exception of 'k is larger than the length of the linked list'", () => {
+    let a = new LinkedListNode("Angel Food");
+    let b = new LinkedListNode("Bundt");
+    let c = new LinkedListNode("Cheese");
+    let d = new LinkedListNode("Devil's Food");
+    let e = new LinkedListNode("Eccles");
+
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    d.next = e;
+
+    expect(() => kthToLastNode(10, a)).to.throw(
+      "k is larger than the length of the linked list: 10"
+    );
+  });
+
+  it("should throw exception of 'Impossible to find less than first to last node'", () => {
+    let a = new LinkedListNode("Angel Food");
+    let b = new LinkedListNode("Bundt");
+    let c = new LinkedListNode("Cheese");
+    let d = new LinkedListNode("Devil's Food");
+    let e = new LinkedListNode("Eccles");
+
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    d.next = e;
+
+    expect(() => kthToLastNode(0, a)).to.throw(
+      "Impossible to find less than first to last node: 0"
+    );
   });
 });
