bfe8084b75bb8cdb7a354209f1f65958dc2df829
Marek Dano <mk.dano@gmail.com>
2018-03-19 18:17:44 +0000

    Add solution to the 'fromLast' problem.

--- StephenGriderInterviewQuestions/fromLast/index.js
+++ StephenGriderInterviewQuestions/fromLast/index.js
@@ -11,6 +11,33 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+  // let counter = 0;
+
+  // while (fast) {
+  //   if (counter <= n) {
+  //     fast = fast.next;
+  //   } else {
+  //     slow = slow.next;
+  //     fast = fast.next;
+  //   }
+  //   counter++;
+  // }
+
+  // OR
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
--- StephenGriderInterviewQuestions/fromLast/test.js
+++ StephenGriderInterviewQuestions/fromLast/test.js
@@ -1,20 +1,44 @@
-const fromLast = require('./index');
-const L = require('./linkedlist');
+const fromLast = require("./index");
+const L = require("./linkedlist");
 const List = L.LinkedList;
 const Node = L.Node;
 
-test('fromLast is a function', () => {
-  expect(typeof fromLast).toEqual('function');
+test("fromLast is a function", () => {
+  expect(typeof fromLast).toEqual("function");
 });
 
-test('fromLast returns the node n elements from the end', () => {
+test("fromLast returns the node 3rd elements from the end", () => {
   const l = new List();
 
-  l.insertLast('a');
-  l.insertLast('b');
-  l.insertLast('c');
-  l.insertLast('d');
-  l.insertLast('e');
+  l.insertLast("a");
+  l.insertLast("b");
+  l.insertLast("c");
+  l.insertLast("d");
+  l.insertLast("e");
 
-  expect(fromLast(l, 3).data).toEqual('b');
+  expect(fromLast(l, 3).data).toEqual("b");
+});
+
+test("fromLast returns the node 4th elements from the end", () => {
+  const l = new List();
+
+  l.insertLast("a");
+  l.insertLast("b");
+  l.insertLast("c");
+  l.insertLast("d");
+  l.insertLast("e");
+
+  expect(fromLast(l, 4).data).toEqual("a");
+});
+
+test("fromLast returns the node zero elements from the end", () => {
+  const l = new List();
+
+  l.insertLast("a");
+  l.insertLast("b");
+  l.insertLast("c");
+  l.insertLast("d");
+  l.insertLast("e");
+
+  expect(fromLast(l, 0).data).toEqual("e");
 });
