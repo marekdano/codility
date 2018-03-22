cf8851d2c7fb3de14acfab163176053f3354c942
Marek Dano <mk.dano@gmail.com>
2018-03-22 11:51:02 +0000

    Describe a new problem of finding the second largest element in BST.

---
+++ InterviewCake/10.SecondLargestInBST.js
@@ -0,0 +1,46 @@
+/**
+ * Write a function to find the 2nd largest element in a binary search tree.
+ *
+ * Here's a sample binary tree node class in ES6:
+ *
+ * class BinaryTreeNode {
+ * 		constructor(value) {
+ * 			this.value = value;
+ * 			this.left = null;
+ * 			this.right = null;
+ * 		}
+ *
+ * 		insertLeft(value) {
+ * 			this.left = new BinaryTreeNode(value);
+ *  		return this.left;
+ * 		}
+ *
+ * 		insertRight(value) {
+ * 			this.right = new BinaryTreeNode(value);
+ *   		return this.right;
+ * 		}
+ * }
+ *
+ * in ES5:
+ *
+ *	function BinaryTreeNode(value) {
+ *   	this.value = value;
+ *   	this.left  = null;
+ *   	this.right = null;
+ *	}
+ *
+ *  BinaryTreeNode.prototype.insertLeft = function(value) {
+ *  	this.left = new BinaryTreeNode(value);
+ *   	return this.left;
+ *   };
+ *
+ *  BinaryTreeNode.prototype.insertRight = function(value) {
+ *    this.right = new BinaryTreeNode(value);
+ *    return this.right;
+ *  };
+ *
+ */
+
+let findSecondLargestElem = str => {};
+
+module.exports = findSecondLargestElem;
