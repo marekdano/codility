1ca45b4753d1a8f182a59b44ead68782baa0d6d9
Marek Dano <mk.dano@gmail.com>
2018-03-27 08:43:34 +0100

    Add solution to the 'levelwidth' problem.

--- StephenGriderInterviewQuestions/levelwidth/index.js
+++ StephenGriderInterviewQuestions/levelwidth/index.js
@@ -11,6 +11,33 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const counters = [0];
+  const arr = [root, "last"];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === "last") {
+      counters.push(0);
+      arr.push("last");
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
+
+// traverseBF(fn) {
+// 	const arr = [this.root];
+// 	while (arr.length) {
+// 		const node = arr.shift();
+
+// 		arr.push(...node.children);
+// 		fn(node);
+// 	}
+// }
 
 module.exports = levelWidth;
