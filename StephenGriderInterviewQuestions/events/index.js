d0e3c4bc0a070b8605d4073882517b1cf56fd2bc
Marek Dano <mk.dano@gmail.com>
2018-04-05 10:12:25 +0200

    Define events problem with test cases.

---
+++ StephenGriderInterviewQuestions/events/index.js
@@ -0,0 +1,8 @@
+// --- Directions
+// Create an 'eventing' library out of the
+// Events class.  The Events class should
+// have methods 'on', 'trigger', and 'off'.
+
+class Events {}
+
+module.exports = Events;
