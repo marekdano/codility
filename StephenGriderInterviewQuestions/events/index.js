551bd102bd6f7b1bb2764bfef9299aff0d2653b5
Marek Dano <mk.dano@gmail.com>
2018-04-06 23:02:25 +0100

    Add solution to the events problem.

--- StephenGriderInterviewQuestions/events/index.js
+++ StephenGriderInterviewQuestions/events/index.js
@@ -7,6 +7,32 @@ class Events {
   constructor() {
     this.events = {};
   }
+
+  // Register an event handler
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
+
+  // Trigger all callback associated
+  // with a given eventName
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (let cb of this.events[eventName]) {
+        cb();
+      }
+    }
+  }
+
+  // Remove all event handlers associated
+  // with the given eventName
+  off(eventName) {
+    //this.events[eventName] = [];
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
