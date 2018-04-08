324452bfc8ca44f3cf0973a5bde3767867cc437b
Marek Dano <mk.dano@gmail.com>
2018-04-08 09:48:38 +0100

    Add solution to the ReverseWords problem.

--- InterviewCake/27.ReverseWords.js
+++ InterviewCake/27.ReverseWords.js
@@ -4,6 +4,7 @@
  * The message has been mostly deciphered, but all the words are backwards! Your colleagues have handed off the last step to you.
  *
  * Write a function reverseWords() that takes a message as an array of characters and reverses the order of the words in-place.
+ * An in-place algorithm operates directly on its input and changes it, instead of creating and returning a new object.
  *
  * For example:
  *
@@ -18,4 +19,42 @@
  * When writing your function, assume the message contains only letters and spaces, and all words are separated by one space.
  */
 
-let reverseWord = message => {};
+let reverseWords = message => {
+  // first we reverse all the characters in the entire message
+  reverseCharacters(message, 0, message.length - 1);
+  // this gives us the right word order
+  // but with each word backwards
+
+  // now we'll make the words forward again
+  // by reversing each word's characters
+
+  // we hold the index of the *start* of the current word
+  // as we look for the *end* of the current word
+  let currentWordStartIndex = 0;
+  for (var i = 0; i <= message.length; i++) {
+    // found the end of the current word!
+    if (i === message.length || message[i] === " ") {
+      // if we haven't exhausted the string our
+      // next word's start is one character ahead
+      reverseCharacters(message, currentWordStartIndex, i - 1);
+      currentWordStartIndex = i + 1;
+    }
+  }
+
+  return message.join("");
+};
+
+let reverseCharacters = (message, leftIndex, rightIndex) => {
+  // walk towards the middle, from both sides
+  while (leftIndex < rightIndex) {
+    // swap the left char and right char
+    [message[rightIndex], message[leftIndex]] = [
+      message[leftIndex],
+      message[rightIndex]
+    ];
+    leftIndex++;
+    rightIndex--;
+  }
+};
+
+module.exports = reverseWords;
