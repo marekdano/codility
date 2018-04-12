b8a4b775b65a0c4d7f9f92629590a90caed8db6c
Marek Dano <mk.dano@gmail.com>
2018-04-12 20:23:37 +0100

    Add solution to the bubbleSort problem.

--- StephenGriderInterviewQuestions/sorting/index.js
+++ StephenGriderInterviewQuestions/sorting/index.js
@@ -2,7 +2,14 @@
 
 function bubbleSort(arr) {
   // Implement bubblesort
-  // return the sorted array
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = { bubbleSort };
--- StephenGriderInterviewQuestions/sorting/test.js
+++ StephenGriderInterviewQuestions/sorting/test.js
@@ -1,4 +1,4 @@
-const S = require('./index');
+const S = require("./index").default;
 const bubbleSort = S.bubbleSort;
 
 function getArray() {
@@ -9,27 +9,27 @@ function getSortedArray() {
   return [-124, -40, 0, 7, 21, 100, 500];
 }
 
-describe('Bubble sort', () => {
-  test('sorts an array', () => {
+describe("Bubble sort", () => {
+  test("sorts an array", () => {
     expect(bubbleSort(getArray())).toEqual(getSortedArray());
   });
 });
 
-describe('Selection sort', () => {
-  test('sorts an array', () => {
+describe("Selection sort", () => {
+  test("sorts an array", () => {
     expect(selectionSort(getArray())).toEqual(getSortedArray());
   });
 });
 
-describe('Merge sort', () => {
-  test('merge function can join together two sorted arrays', () => {
+describe("Merge sort", () => {
+  test("merge function can join together two sorted arrays", () => {
     const left = [1, 10];
     const right = [2, 8, 12];
 
-    expect(merge(left, right)).toEqual([1,2,8,10,12]);
+    expect(merge(left, right)).toEqual([1, 2, 8, 10, 12]);
   });
 
-  test('sorts an array', () => {
+  test("sorts an array", () => {
     expect(mergeSort(getArray())).toEqual(getSortedArray());
   });
 });
