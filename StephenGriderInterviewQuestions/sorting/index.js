b4efb808cd603b4bf3260576cc6102b1e8c1286e
Marek Dano <mk.dano@gmail.com>
2018-04-16 21:20:22 +0100

    Add solution to the merge sorting problem.

--- StephenGriderInterviewQuestions/sorting/index.js
+++ StephenGriderInterviewQuestions/sorting/index.js
@@ -28,4 +28,17 @@ function selectionSort(arr) {
   return arr;
 }
 
-module.exports = { bubbleSort, selectionSort };
+function merge(left, right) {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  return [...result, ...left, ...right];
+}
+
+module.exports = { bubbleSort, selectionSort, merge };
