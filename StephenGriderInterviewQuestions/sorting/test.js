0151020382d76a175e53bba60b07f0a2bba16815
Marek Dano <mk.dano@gmail.com>
2018-04-16 21:19:11 +0100

    Add test cases to the merge sorting problem.

--- StephenGriderInterviewQuestions/sorting/test.js
+++ StephenGriderInterviewQuestions/sorting/test.js
@@ -1,6 +1,7 @@
 const S = require("./index");
 const bubbleSort = S.bubbleSort;
 const selectionSort = S.selectionSort;
+const merge = S.merge;
 
 function getArray() {
   return [100, -40, 500, -124, 0, 21, 7];
@@ -23,11 +24,18 @@ describe("Selection sort", () => {
 });
 
 describe("Merge sort", () => {
-  test("merge function can join together two sorted arrays", () => {
+  test(" can merge two sorted arrays when second array is longer than first one", () => {
     const left = [1, 10];
     const right = [2, 8, 12];
 
     expect(merge(left, right)).toEqual([1, 2, 8, 10, 12]);
+  });
+
+  test("can merge two sorted arrays when first array is longer than second one", () => {
+    const left2 = [1, 10, 11, 13];
+    const right2 = [-2, 8, 12];
+
+    expect(merge(left2, right2)).toEqual([-2, 1, 8, 10, 11, 12, 13]);
   });
 
   test("sorts an array", () => {
