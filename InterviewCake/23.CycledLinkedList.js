23a684051d46cdc9045534ceef0d504d50096eed
Marek Dano <mk.dano@gmail.com>
2018-04-21 20:11:32 +0100

    Add solution to the 23.CycledLinkedList problem.

--- InterviewCake/23.CycledLinkedList.js
+++ InterviewCake/23.CycledLinkedList.js
@@ -1,27 +1,41 @@
 /**
  * You have a singly-linked list and want to check if it contains a cycle.
  * A singly-linked list is built with nodes, where each node has:
- * 
+ *
  * 	node.next — the next node in the list.
- * 	node.value — the data held in the node. For example, if our linked list stores people 
+ * 	node.value — the data held in the node. For example, if our linked list stores people
  * 							 in line at the movies, node.value might be the person's name.
- * 
+ *
  * For example:
  *
  *  function LinkedListNode(value) {
  *  	this.value = value;
  *    this.next = null;
  *  }
- * 
- * A cycle occurs when a node’s next points back to a previous node in the list. 
+ *
+ * A cycle occurs when a node’s next points back to a previous node in the list.
  * The linked list is no longer linear with a beginning and end—instead, it cycles through a loop of nodes.
- * 
- * Write a function containsCycle() that takes the first node in a singly-linked list 
+ *
+ * Write a function containsCycle() that takes the first node in a singly-linked list
  * and returns a boolean indicating whether the list contains a cycle.
  */
 
-let containsCycle = (linkedList) => {
-	return true
+let containsCycle = linkedList => {
+  // start both runners at the beginning
+  let slowRunner = linkedList.next;
+  let fastRunner = linkedList.next.next;
+
+  // until we hit the end of the list
+  while (fastRunner && fastRunner.next) {
+    slowRunner = slowRunner.next;
+    fastRunner = fastRunner.next.next;
+
+    if (slowRunner === fastRunner) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 module.exports = containsCycle;
