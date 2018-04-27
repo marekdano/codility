0df1a669a524988f324084a7fcf6d3b3dec8f8a5
Marek Dano <mk.dano@gmail.com>
2018-04-28 00:05:10 +0100

    Add solution to the problem of 20.GetMaxFromStack.

--- InterviewCake/20.GetMaxFromStack.js
+++ InterviewCake/20.GetMaxFromStack.js
@@ -21,12 +21,21 @@ function MaxStack() {
 // the new max! So we'll add it to maxesStack.
 MaxStack.prototype.push = function(item) {
   this.stack.push(item);
+  if (!this.maxesStack.peek() || item >= this.maxesStack.peek()) {
+    this.maxesStack.push(item);
+  }
 };
 
 // Remove and return the top item from our stack. If it equals
 // the top item in maxesStack, they must have been pushed in together.
 // So we'll pop it out of maxesStack too.
-MaxStack.prototype.pop = function() {};
+MaxStack.prototype.pop = function() {
+  const item = this.stack.pop();
+  if (item === this.maxesStack.peek()) {
+    this.maxesStack.pop();
+  }
+  return item;
+};
 
 // The last item in maxesStack is the max item in our stack.
 MaxStack.prototype.getMax = function() {
