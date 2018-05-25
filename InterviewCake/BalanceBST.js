987aa66896431ea8fa4fbba57975aabbd71c7fa0
Marek Dano <mk.dano@gmail.com>
2018-05-25 19:25:13 +0100

    Add description to the problem of BalanceBST.

---
+++ InterviewCake/BalanceBST.js
@@ -0,0 +1,11 @@
+/**
+ * Write a function to see if a binary tree is "superbalanced"
+ * (a new tree property we just made up).
+ * 
+ * A tree is "superbalanced" if the difference between 
+ * the depths of any two leaf nodes ↴ is no greater than one.
+ */
+
+let isBalanced = (treeRoot) => {}
+
+module.exports = isBalanced;
