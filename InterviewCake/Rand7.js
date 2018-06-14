91ac20a5ce931a08a03c7b94d1962b9dd8191fc4
Marek Dano <mk.dano@gmail.com>
2018-06-14 16:21:40 +0100

    Describe problem of Rand7.

---
+++ InterviewCake/Rand7.js
@@ -0,0 +1,12 @@
+/**
+ * You have a function rand5() that generates a random integer from 1 to 5.
+ * Use it to write a function rand7() that generates a random integer from 1 to 7.
+ */
+
+const rand7 = () => {};
+
+const rand5 = () => {
+  return Math.ceil(Math.random() * 5);
+};
+
+module.exports = rand7;
