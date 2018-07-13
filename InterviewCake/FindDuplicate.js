ba30d37ee3d4ae33e56181138cf183bc4d796a3e
Marek Dano <mk.dano@gmail.com>
2018-07-13 23:56:16 +0100

    Add solution to the problem of FindDuplicate.

--- InterviewCake/FindDuplicate.js
+++ InterviewCake/FindDuplicate.js
@@ -16,6 +16,52 @@
 
 // SOLUTION should be O(n lg n) time and O(1) space.
 
-const findDuplicate = {}
+const findDuplicate = (theArray) => {
+
+	let floor = 1;
+	let ceiling = theArray.length - 1;
+
+	while (floor < ceiling) {
+
+		// divide our range 1..n into an upper range and lower range
+		// (such that they don't overlap)
+		// lower range is floor..midpoint
+		// upper range is midpoint+1..ceiling
+		const midpoint = Math.floor(floor + ((ceiling - floor) / 2));
+		const lowerRangeFloor   = floor;
+		const lowerRangeCeiling = midpoint;
+		const upperRangeFloor   = midpoint + 1;
+		const upperRangeCeiling = ceiling;
+
+		const distinctPossibleIntegersInLowerRange = lowerRangeCeiling - lowerRangeFloor + 1;
+
+		// count number of items in lower range
+		const itemsInLowerRange = 0;
+		theArray.forEach(function(item) {
+			// is it in the lower range?
+			if (item >= lowerRangeFloor && item <= lowerRangeCeiling) {
+				itemsInLowerRange += 1;
+			}
+		});
+
+		if (itemsInLowerRange > distinctPossibleIntegersInLowerRange) {
+
+			// there must be a duplicate in the lower range
+			// so use the same approach iteratively on that range
+			floor   = lowerRangeFloor;
+			ceiling = lowerRangeCeiling;
+		} else {
+
+			// there must be a duplicate in the upper range
+			// so use the same approach iteratively on that range
+			floor   = upperRangeFloor;
+			ceiling = upperRangeCeiling;
+		}
+	}
+
+	// floor and ceiling have converged
+	// we found a number that repeats!
+	return floor;
+}
 
 module.exports = findDuplicate;
