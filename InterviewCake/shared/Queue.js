925a4f78a9c429a435cff49ee7e00c2ad3fb651a
Marek Dano <mk.dano@gmail.com>
2018-08-05 14:11:08 +0200

    Define data structure of Queue in the shared folder.

---
+++ InterviewCake/shared/Queue.js
@@ -0,0 +1,23 @@
+function Queue() {
+	this.data = [];
+}
+
+Queue.prototype.enqueue = function(record) {
+	this.data.unshift(record);
+}
+
+Queue.prototype.dequeue = function() {
+	this.data.pop();
+}
+
+Queue.prototype.first = function() {
+  return this.data[0];
+}
+Queue.prototype.last = function() {
+  return this.data[this.data.length - 1];
+}
+Queue.prototype.size = function() {
+  return this.data.length;
+}
+
+module.exports = Queue;
