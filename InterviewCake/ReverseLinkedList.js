42deca796daf01164060d91b8a43217f56855abb
Marek Dano <mk.dano@gmail.com>
2018-08-29 09:23:04 +0100

    Add solution to the ReverseLinkedList problem.

--- InterviewCake/ReverseLinkedList.js
+++ InterviewCake/ReverseLinkedList.js
@@ -16,4 +16,36 @@
  * 
  */
 
-const reverse = (headOfList) => {return; };
+ // COMPLEXITY
+ // O(n) time and O(1) space. We pass over the list only once, and maintain a constant number of variables in memory.
+
+ // We're careful to copy currentNode.next into nextNode before setting currentNode.next to previousNode. 
+ // Otherwise "stepping forward" at the end could actually mean stepping back to previousNode!
+ // We return previousNode because when we exit the list, currentNode is null. 
+ // Which means that the last node we visited—previousNode—was the tail of the original list, 
+ // and thus the head of our reversed list.
+
+const reverse = (headOfList) => {
+	let currentNode = headOfList;
+  let previousNode = null;
+  let nextNode = null;
+
+  // Until we have 'fallen off' the end of the list
+  while (currentNode) {
+
+    // Copy a pointer to the next element
+    // before we overwrite currentNode.next
+    nextNode = currentNode.next;
+
+    // Reverse the 'next' pointer
+    currentNode.next = previousNode;
+
+    // Step forward in the list
+    previousNode = currentNode;
+    currentNode = nextNode;
+  }
+
+  return previousNode;
+};
+
+module.exports = reverse;
