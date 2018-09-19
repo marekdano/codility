c5599e1d74a5b2fad5ed94f09f61cb616ffaab1e
Marek Dano <mk.dano@gmail.com>
2018-09-19 22:53:38 +0100

    Add description to the problem of fibonacci number.

---
+++ InterviewCake/FibonacciNumber.js
@@ -0,0 +1,20 @@
+/**
+ * Write a function fib() that takes an integer n 
+ * and returns the nth Fibonacci number.
+ * 
+ * Let's say our Fibonacci series is 0-indexed and starts with 0. 
+ * So:
+ * 
+ *  fib(0);  // => 0
+ *  fib(1);  // => 1
+ *  fib(2);  // => 1 
+ *  fib(3);  // => 2
+ *  fib(4);  // => 3
+ *  ...
+ * 
+ * Try to do this with O(n) time and O(1) space.
+ */
+
+const fib = (n) => {};
+
+module.exports = fib;
