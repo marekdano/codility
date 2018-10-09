df5dbbc5ef58c67bc9c9b266a2dba8ca0bcb1a93
Marek Dano <mk.dano@gmail.com>
2018-10-09 23:12:36 +0100

    Add description to the new problem of DeleteNodeFromLinkedList.

---
+++ InterviewCake/DeleteNodeFromLinkedList.js
@@ -0,0 +1,25 @@
+/**
+ * Delete a node from a singly-linked list, given only a variable pointing to that node.
+ * 
+ * The input could, for example, be the variable b below:
+ * 
+ * 	class LinkedListNode {
+ * 		constructor(value) {
+ * 			this.value = value;
+ * 			this.next = null;
+ * 		}
+ * 	}
+ * 
+ * 	const a = new LinkedListNode('A');
+ * 	const b = new LinkedListNode('B');
+ * 	const c = new LinkedListNode('C');
+ * 	
+ * 	a.next = b;
+ * 	b.next = c;
+ * 
+ * 	deleteNode(b);
+ */
+
+const deleteNode = (node) => {};
+
+module.exports = deleteNode;
