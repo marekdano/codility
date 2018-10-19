4d709b28784855b7a6d5cacff5da148535bd8f23
Marek Dano <mk.dano@gmail.com>
2018-10-19 22:01:30 +0100

    Add description to the problem of ShuffleArray.

---
+++ InterviewCake/ShuffleArray.js
@@ -0,0 +1,13 @@
+/**
+ * Write a function for doing an in-place shuffle of an array.
+ * 
+ * The shuffle must be "uniform," meaning each item in the original array 
+ * must have the same probability of ending up in each spot in the final array.
+ * 
+ * Assume that you have a function getRandom(floor, ceiling) 
+ * for getting a random integer that is >= floor and <= ceiling.
+ */
+
+const shuffle = (array) => {};
+
+module.exports = shuffle;
