afee5c095675bc8b53d1fd65afc787fb8218c905
Marek Dano <mk.dano@gmail.com>
2018-10-20 23:50:40 +0100

    Add solution to the ShuffleArray problem.

--- InterviewCake/ShuffleArray.js
+++ InterviewCake/ShuffleArray.js
@@ -8,11 +8,37 @@
  * for getting a random integer that is >= floor and <= ceiling.
  */
 
+// Complexity 
+// O(n) time and O(1) space
+
 const getRandom = (floor, ceiling) => {
 	return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
 }
 
+const shuffle = (array) => {
+	// if it's 1 or 0 items, just return
+	if (array.length <= 1) return;
 
-const shuffle = (array) => {};
+	// walk through from beginning to end
+	for (var indexWeAreChoosingFor = 0;
+		indexWeAreChoosingFor < array.length - 1; indexWeAreChoosingFor++) {
+
+		// choose a random not-yet-placed item to place there
+		// (could also be the item currently in that spot)
+		// must be an item AFTER the current item, because the stuff
+		// before has all already been placed
+		var randomChoiceIndex = getRandom(indexWeAreChoosingFor, array.length - 1);
+
+		// place our random choice in the spot by swapping
+		if (randomChoiceIndex !== indexWeAreChoosingFor) {
+			var valueAtIndexWeChoseFor = array[indexWeAreChoosingFor];
+			array[indexWeAreChoosingFor] = array[randomChoiceIndex];
+			array[randomChoiceIndex] = valueAtIndexWeChoseFor;
+			
+			// ES6
+			// [ array[indexWeAreChoosingFor], array[randomChoiceIndex] ] = [ array[randomChoiceIndex], array[indexWeAreChoosingFor] ];
+		}
+	}
+};
 
 module.exports = shuffle;
