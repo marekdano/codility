03d34606179cc5634c6afeb74371ab3d5989cc22
Marek Dano <mk.dano@gmail.com>
2019-01-05 12:39:36 +0100

    Add description to the problem of ExcelSheetColumnNumber.

---
+++ Mix/ExcelSheetColumnNumber.js
@@ -0,0 +1,22 @@
+/**
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * 
+ * For example:
+ * 	A -> 1
+ *  B -> 2
+ *  C -> 3
+ *  ...
+ *  Z -> 26
+ *  AA -> 27 
+ *  AB -> 28 
+ * ...
+ * 
+ *  Examples:
+ * 		"A" -> 1
+ * 		"AB" -> 28
+ * 		"ZY" -> 701
+ */
+
+const titleToNumber = (title) => {};
+
+module.exports = titleToNumber;
