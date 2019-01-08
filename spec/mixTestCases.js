5ad3674c94e62a1fa14ceb367ca12e1eb9792647
Marek Dano <mk.dano@gmail.com>
2019-01-08 20:58:50 +0000

    Add description and test cases for the problem of FindUniqueCharInString.

--- spec/mixTestCases.js
+++ spec/mixTestCases.js
@@ -2,6 +2,7 @@ let path = require('path');
 let expect = require('chai').expect;
 
 let titleToNumber = require(path.join(__dirname, '../Mix/ExcelSheetColumnNumber'));
+let firstUniqChar = require(path.join(__dirname, '../Mix/FirstUniqueCharInString'));
 
 describe('titleToNumber function', () => {
   it('exists', () => {
@@ -18,5 +19,19 @@ describe('titleToNumber function', () => {
 
 	it('should return 701 from title of "ZY"', () => {
 		expect(titleToNumber('ZY')).to.equal(701);
+	});
+});
+
+describe('firstUniqChar function', () => {
+  it('exists', () => {
+    expect(firstUniqChar).to.be.a('function');
+	});
+
+	it('should return 0 from string of "leetcode"', () => {
+		expect(firstUniqChar('leetcode')).to.equal(0);
+	});
+
+	it('should return 2 from string of "loveleetcode"', () => {
+		expect(firstUniqChar('loveleetcode')).to.equal(2);
 	});
 });
