32b7b6ae20f7bc24d6c36413c324f0b74d7250bc
Marek Dano <mk.dano@gmail.com>
2020-08-28 13:53:42 +0100

    Add solution for sumTwoNumbers

--- DailyInterviewPro/SumTwoNumbers.js
+++ DailyInterviewPro/SumTwoNumbers.js
@@ -3,9 +3,13 @@
  * You are given a list of numbers, and a target number k. Return whether or not there are two numbers in the list that add up to k.
  * 
  * Example:
- * 		Given [4, 7, 1 , -3, 2] and k = 5, return "true" since 4 + 1 = 5.
+ * 		Given [4, 7, 1, -3, 2] and k = 5, return "true" since 4 + 1 = 5.
  */
 
 export const sumTwoNumbers = (array, k) => {
-	return null
+	const differenceNums = []
+	array.forEach(num => differenceNums.push(k-num))
+	const allNums = [...array, ...differenceNums]
+
+	return [...new Set(allNums)].length !== allNums.length
 }
