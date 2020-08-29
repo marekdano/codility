85f69c572da9e0bd68770a87e83ac03658bcc94d
Marek Dano <mk.dano@gmail.com>
2020-08-29 11:13:31 +0100

    Add solution to matrix problem

--- StephenGriderInterviewQuestions/matrix/index.js
+++ StephenGriderInterviewQuestions/matrix/index.js
@@ -3,8 +3,8 @@
 // and returns a NxN spiral matrix.
 // --- Examples
 //   matrix(2)
-//     [[undefined, undefined],
-//     [undefined, undefined]]
+//     [[1, 2],
+//     [4, 3]]
 //   matrix(3)
 //     [[1, 2, 3],
 //     [8, 9, 4],
@@ -15,6 +15,47 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+export default function matrix(n) {
+	const results = []
 
-module.exports = matrix;
+	for(let i = 0; i < n; i++) {
+		results.push([])
+	}
+
+	let counter = 1
+	let startColumn = 0
+	let endColumn = n-1
+	let startRow = 0
+	let endRow = n-1
+
+	while(startColumn <= endColumn && startRow <= endRow) {
+		// top row
+		for (let i = startColumn; i <= endColumn; i++) {
+			results[startRow][i] = counter
+			counter++
+		}
+		startRow++
+
+		// right column
+		for (let i = startRow; i <= endRow; i++) {
+			results[i][endColumn] = counter
+			counter++
+		}
+		endColumn--
+
+		// bottom row
+		for (let i = endColumn; i >= startColumn; i--) {
+			results[endRow][i] = counter
+			counter++
+		}
+		endRow--
+
+		// start column
+		for (let i = endRow; i >= startRow; i--) {
+			results[i][startColumn] = counter
+			counter++
+		}
+		startColumn++
+	}
+	return results
+}
--- StephenGriderInterviewQuestions/matrix/test.js
+++ StephenGriderInterviewQuestions/matrix/test.js
@@ -1,4 +1,4 @@
-const matrix = require("./index");
+import matrix from "./";
 
 test("matrix is a function", () => {
   expect(typeof matrix).toEqual("function");
@@ -27,3 +27,23 @@ test("matrix produces a 4x4 array", () => {
   expect(m[2]).toEqual([11, 16, 15, 6]);
   expect(m[3]).toEqual([10, 9, 8, 7]);
 });
+
+test("matrix produces a 4x4 array", () => {
+  const m = matrix(4);
+  expect(m.length).toEqual(4);
+  expect(m[0]).toEqual([1, 2, 3, 4]);
+  expect(m[1]).toEqual([12, 13, 14, 5]);
+  expect(m[2]).toEqual([11, 16, 15, 6]);
+  expect(m[3]).toEqual([10, 9, 8, 7]);
+});
+
+test("matrix produce a 6x6 array with spiral numbers", () => {
+  const m = matrix(6);
+  expect(m.length).toEqual(6);
+  expect(m[0]).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(m[1]).toEqual([20, 21, 22, 23, 24, 7]);
+  expect(m[2]).toEqual([19, 32, 33, 34, 25, 8]);
+  expect(m[3]).toEqual([18, 31, 36, 35, 26, 9]);
+  expect(m[4]).toEqual([17, 30, 29, 28, 27, 10]);
+  expect(m[5]).toEqual([16, 15, 14, 13, 12, 11]);
+})
