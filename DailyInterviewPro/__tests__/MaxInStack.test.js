f096fa5ed289d0301cc7d1cb9f033ac964721f88
Marek Dano <mk.dano@gmail.com>
2020-11-03 10:09:20 +0000

    Add unit tests for the MaxInStack problem

---
+++ DailyInterviewPro/__tests__/MaxInStack.test.js
@@ -0,0 +1,29 @@
+import { MaxStack } from '../MaxInStack'
+
+describe('MaxStack', () => {
+	it('should return "null" when stack is empty', () => {
+		const stack = new MaxStack()
+		expect(stack.max()).toBe(null)
+
+		stack.push(1)
+		stack.push(2)
+		stack.pop()
+		stack.pop()
+		expect(stack.max()).toBe(null)
+	})
+
+	it('should return max (3 and 2) when stack is NOT empty', () => {
+		const stack = new MaxStack()
+		stack.push(1)
+		stack.push(2)
+		stack.push(3)
+		stack.push(2)
+		expect(stack.max()).toBe(3)
+		stack.pop()
+		stack.pop()
+		expect(stack.max()).toBe(2)
+		stack.pop()
+		stack.pop()
+		expect(stack.max()).toBe(null)
+	})
+})
