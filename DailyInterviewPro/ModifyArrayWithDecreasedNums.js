c90d072e2a792fa2eb6a31ef4a1de4ce7cb49bfe
Marek Dano <mk.dano@gmail.com>
2020-11-12 22:59:48 +0000

    Add solution to 'ModifyArrayWithDecreaseNums'"

--- DailyInterviewPro/ModifyArrayWithDecreasedNums.js
+++ DailyInterviewPro/ModifyArrayWithDecreasedNums.js
@@ -10,6 +10,38 @@
  * 		[13, 4, 1] however, should return false, since there is no way to modify just one element to make the array non-decreasing.
  */
 
-export const check = (array) => {
-	return null
+export const check = (arr) => {
+	const len = arr.length
+	let modifiedCount = 0
+
+	// check if the last element needs 
+	// to be modified or not
+	if (arr[len - 1] >= arr[len - 2]) {
+		arr[len - 1] = arr[len - 2] - 1
+		modifiedCount++
+	}
+
+	// check if the first element needs 
+	// to be modified or not
+	if (arr[0] <= arr[1]) {
+		arr[0] = arr[1] + 1
+		modifiedCount++
+	}
+
+	// loop from 2nd element to the 2nd last one
+	for (let i = len - 2; i > 0; i--) {
+		if ((arr[i - 1] <= arr[i] && arr[i + 1] <= arr[i])
+			|| (arr[i - 1] >= arr[i] && arr[i + 1] >= arr[i])) {
+				arr[i] = (arr[i - 1] + arr[i + 1]) / 2
+				modifiedCount++
+			}
+
+			if (arr[i] === arr[i - 1] || arr[i] === arr[i + 1]) {
+				return false 
+			}
+	}
+
+	if (modifiedCount > 1) return false
+
+	return true
 }
--- DailyInterviewPro/__tests__/ModifyArrayWithDecreasedNums.test.js
+++ DailyInterviewPro/__tests__/ModifyArrayWithDecreasedNums.test.js
@@ -3,11 +3,11 @@ import { check } from '../ModifyArrayWithDecreasedNums'
 describe('Array check', () => {
 	test('should returns true when replacing a single item decreases item numbers in array', () => {
 		const arr = [13, 4, 7]
-		expect(arr).toBeTruthy()
+		expect(check(arr)).toBeTruthy()
 	})
 	
 	test('should return false when none or more than a single number in array has to be replace', () => {
-		const arr = [5, 1, 3, 2, 5]
-		expect(arr).toBeFalsy()
+		const arr = [4, 1, 2, 3, 5]
+		expect(check(arr)).toBeFalsy()
 	})
 })
