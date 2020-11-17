f76d45be7a277c6fba994aef769d398fe5dd9d63
Marek Dano <mk.dano@gmail.com>
2020-11-17 22:55:17 +0000

    Fix failing tests in the 'WordSearch' problem

--- DailyInterviewPro/WordSearch.js
+++ DailyInterviewPro/WordSearch.js
@@ -7,55 +7,44 @@
 */
 
 const wordSearch = (matrix, word) => {
-	const search = (matrix, row, col, word) => {
-		// if first character of word doesn't match 
-    // with given starting point in grid. 
-    if (matrix[row][col] !== word[0]) { 
-			return false; 
+	const search = (matrix, word, row, col) => {
+		const len = word.length
+		let str = ''
+		
+		if (matrix[row][col] !== word[0]) {
+			return false
 		}
 
-		const wordLength = word.length
-		let k
-
-		for (k = 1; k < wordLength; k++) {
-			for (let r = row; r < matrix[0].length; r++) {
-				// if the letter in next row is not next letter in the word 
-				if (matrix[r][col] !== word[k]) {
-					break
-				}
-			} 
-
-			for (let c = col; c < matrix.length; c++) {
-				// if the letter in next column is not next letter in the word 
-				if (matrix[row][c] !== word[k]) {
-					break
-				}
-			} 
+		// check horizontally
+		for (let c = col; c < matrix[0].length; c++) {
+			str += matrix[row][c]
 		}
 
-		if (k === wordLength) {
-			return true
+		if (str.includes(word)) return true
+		
+		str = ''
+		// check vertically
+		for (let r = row; r < matrix.length; r++) {
+			str += matrix[r][col]
 		}
+
+		if (str.includes(word)) return true
+		
 		return false
 	}
 
-	const rowLength = matrix[0].length
-	const colLength = matrix.length
+	const rowLength = matrix.length
+	const colLength = matrix[0].length
 	const wordLength = word.length
 	let result = false
-
-	
 
 	if (rowLength < wordLength || colLength < wordLength) {
 		return result
 	}
 
-	
-
 	for (let row = 0; row < rowLength; row++) {
 		for (let col = 0; col < colLength; col++) {
-			if (search(matrix, row, col, word)) {
-				console.log('HERE')
+			if (search(matrix, word, row, col)) {
 				result = true
 				break
 			}
