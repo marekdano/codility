3fad6380e4f8badefe6abd0d642dd75f985e4670
Marek Dano <mk.dano@gmail.com>
2021-02-01 23:35:47 +0000

    Delete problem of 'AnagramInString'

--- DailyInterviewPro/AnagramInString/index.test.js
+++
@@ -1,7 +0,0 @@
-import { findAnagrams } from './index';
-
-describe('findAnagrams', () => {
-  it('returns indexes in string where substring is an anagram', () => {
-    expect(findAnagrams('acdbacdacb', 'abc')).toEq([3, 7]);
-  });
-});
